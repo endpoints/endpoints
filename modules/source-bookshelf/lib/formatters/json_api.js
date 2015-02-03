2c48b449cb97c6fe2c211880ea3da5c7b86cf809
Tyler Kellen <tyler@sleekcode.net>
2015-02-03 12:08:04 -0500

    don't try to link nested relations, and throw when an invalid relation is iterated.

--- modules/source-bookshelf/lib/formatters/json_api.js
+++ modules/source-bookshelf/lib/formatters/json_api.js
@@ -85,9 +85,17 @@ function link (model, relations, exporter) {
 
 function linkBelongsTo (model) {
   return model.constructor.relations.reduce(function (result, relationName) {
-    var relatedData = model.related(relationName).relatedData;
-    var resourceType = relatedData.target.typeName;
     var id, link;
+    var isNestedRelation = relationName.indexOf('.') !== -1;
+    var relation = model.related(relationName);
+    if (isNestedRelation) {
+      return result;
+    }
+    if (!relation) {
+      throw new Error('Relation '+relationName+' is not defined on '+model.tableName);
+    }
+    var relatedData = relation.relatedData;
+    var resourceType = relatedData.target.typeName;
     if (relatedData.type === 'belongsTo') {
       id = model.get(relatedData.foreignKey);
       link = result[relationName] = {
