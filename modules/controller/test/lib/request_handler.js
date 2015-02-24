68c1a3e1d61c10f619708a39c5d7f764b6413dd2
Bob Holt <bobholt@gmail.com>
2015-02-24 16:52:33 -0500

    add controller/lib/request_handler tests

--- modules/controller/test/lib/request_handler.js
+++ modules/controller/test/lib/request_handler.js
@@ -1,14 +1,53 @@
 const expect = require('chai').expect;
+const sinon = require('sinon');
+const bluebird = require('bluebird');
 
 const requestHandler = require('../../lib/request_handler');
 
 describe('requestHandler', function() {
-  var opts = {
-    method: 'method'
-  };
+  var controllerStub, responderStub;
 
-  it('should return a function', function() {
-    expect(requestHandler(opts)).to.be.a('function');
+  beforeEach(function() {
+    controllerStub = sinon.stub().returns(new bluebird.Promise(function(resolve, reject) {
+      resolve();
+    }));
+    responderStub = sinon.stub().returnsThis();
+  });
+
+  it('should return a function for each request type', function() {
+    expect(requestHandler({method:'create'})).to.be.a('function');
+    expect(requestHandler({method:'read'})).to.be.a('function');
+    expect(requestHandler({method:'readRelated'})).to.be.a('function');
+    expect(requestHandler({method:'update'})).to.be.a('function');
+    expect(requestHandler({method:'destroy'})).to.be.a('function');
+  });
+
+  it('should return a handler that calls the passed-in controller', function() {
+    var req = {
+      headers: {
+        accept: 'application/vnd.api+json'
+      }
+    };
+
+    requestHandler({
+      method: 'read',
+      controller: controllerStub
+    })(req, {}, function() {});
+
+    expect(controllerStub.calledOnce).to.be.true;
+  });
+
+  it('should call the passed-in responder on a validation error', function() {
+    requestHandler({
+      method: 'read',
+      controller: controllerStub,
+      payload: function(err) {return err;},
+      responder: responderStub
+    })({headers:{}}, {}, function() {});
+
+    expect(responderStub.calledOnce).to.be.true;
+    expect(responderStub.getCall(0).args[0]).to.be.instanceof(Error);
+    expect(controllerStub.called).to.be.false;
   });
 
 });
