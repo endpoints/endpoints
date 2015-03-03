9b0dad4def75750b87cff88f55c01a0dc82d5436
Tyler Kellen <tyler@sleekcode.net>
2015-03-03 01:43:41 -0800

    don't re-create id filter every time one is used

--- modules/source-bookshelf/lib/process_filter.js
+++ modules/source-bookshelf/lib/process_filter.js
@@ -1,14 +1,16 @@
 const _ = require('lodash');
 
+const idFilter = function (qb, value) {
+  return qb.whereIn('id', value);
+};
+
 module.exports = function (model, query, filterBy) {
   var filters = model.filters;
   return _.transform(filterBy, function (result, value, key) {
     var filter = filters[key];
     value = String(value).split(',');
     if (key === 'id' && !filter) {
-      filter = function (qb, value) {
-        return qb.whereIn('id', value);
-      };
+      filter = idFilter;
     }
     return filter ? filter.call(filters, result, value) : result;
   }, query);
