e71d22bbd262cfb347320fa69abdf2cf29552a6a
Bob Holt <bobholt@gmail.com>
2015-03-04 14:44:16 -0500

    Ensure that if an update is made that refers to a relation that doesn't exist, we return a `404 Not Found` per json-api spec.
    "A server **MUST** return 404 Not Found when processing a request that
    references a related resource that does not exist."
    
    - OMG

--- modules/source-bookshelf/lib/destructure_request_data.js
+++ modules/source-bookshelf/lib/destructure_request_data.js
@@ -1,44 +1,74 @@
-const sanitizeRequestData = require('./sanitize_request_data');
+const bPromise = require('bluebird');
 const Kapow = require('kapow');
+const sanitizeRequestData = require('./sanitize_request_data');
 
 module.exports = function(model, params) {
   if (!params) {
-    return {};
+    return bPromise.resolve({});
   }
 
   var relations = params.links;
   var toManyRels = [];
 
   if (relations) {
-    params = Object.keys(relations).reduce(function(result, key) {
+    return bPromise.reduce(Object.keys(relations), function(result, key) {
       if (!model.related(key)) {
-        throw Kapow(403, 'Unable to update relationships');
+        throw Kapow(404, 'Unable to find relation "' + key + '"');
       }
 
       var fkey;
       var relation = relations[key];
-      var relationType = model.related(key).relatedData.type;
+      var relatedData = model.related(key).relatedData;
+      var relationType = relatedData.type;
 
       // toOne relations
       if (relationType === 'belongsTo' || relationType === 'hasOne') {
-        fkey = model.related(key).relatedData.foreignKey;
-        params[fkey] = relation === null ? relation : relation.id;
+        fkey = relatedData.foreignKey;
+
+        return relatedData.target.collection().query(function(qb) {
+          if (relation === null) {
+            return qb;
+          }
+          return qb.where({id:relation.id});
+        }).fetchOne().then(function(model) {
+          if (model === null) {
+            throw Kapow(404, 'Unable to find relation "' + key + '" with id ' + relation.id);
+          }
+          params[fkey] = relation === null ? relation : relation.id;
+          return params;
+        });
       }
 
       // toMany relations
       if (relationType === 'belongsToMany' || relationType === 'hasMany') {
-        toManyRels.push({
-          name: key,
-          id: relations[key].id
+        return bPromise.map(relations[key].id, function(id) {
+          return relatedData.target.collection().query(function(qb) {
+            return qb.where({id:id});
+          }).fetchOne().then(function(model) {
+            if (model === null) {
+              throw Kapow(404, 'Unable to find relation "' + key + '" with id ' + id);
+            }
+            return params;
+          });
+        }).then(function() {
+          toManyRels.push({
+            name: key,
+            id: relations[key].id
+          });
+          return params;
         });
       }
-
-      return params;
-    }, params);
+    }, params).then(function(params) {
+      return {
+        data: sanitizeRequestData(params),
+        toManyRels: toManyRels
+      };
+    });
   }
 
-  return {
+  return bPromise.resolve({
     data: sanitizeRequestData(params),
     toManyRels: toManyRels
-  };
+  });
+
 };
