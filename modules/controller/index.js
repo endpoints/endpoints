a91f3d2a55951d944e3af708afa3edd00be468bf
Ashley Williams <ashley@bocoup.com>
2015-03-10 14:32:12 -0400

    add first pass at API docs for endpoints.controller

--- modules/controller/index.js
+++ modules/controller/index.js
@@ -1,9 +1,17 @@
 const _ = require('lodash');
-
 const configure = require('./lib/configure');
 const validate = require('./lib/validate');
 const process = require('./lib/process');
 
+/**
+ * Constructor for the Endpoints Controller; Provides methods for generating
+ * request handling functions to connect HTTP requests to data sources. Takes
+ * a single parameter, `opts.source`, contained an endpoints-wrapper around a
+ * given data source.
+ *
+ * @constructor
+ * @param {Object#Source} opts.source - A wrapper object for a data source.
+ */
 function Controller(opts) {
   if (!opts) {
     opts = {};
@@ -14,6 +22,11 @@ function Controller(opts) {
   _.extend(this, opts);
 }
 
+/**
+ * A function for generating CRUD(create, read, update, destroy) methods.
+ *
+ * @param {string} method - The name of the function to be created.
+ */
 Controller.method = function (method) {
   return function (opts) {
     var source = this.source;
@@ -26,9 +39,24 @@ Controller.method = function (method) {
   };
 };
 
+/**
+ * A function that creates an object
+ */
 Controller.prototype.create = Controller.method('create');
+
+/**
+ * A function that retrieves a given object(s).
+ */
 Controller.prototype.read = Controller.method('read');
+
+/**
+ * A function that edits any number of existing fields on an object.
+ */
 Controller.prototype.update = Controller.method('update');
+
+/**
+ * A function that deletes a single object.
+ */
 Controller.prototype.destroy = Controller.method('destroy');
 
 module.exports = Controller;
