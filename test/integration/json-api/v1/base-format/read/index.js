fc4e57668f2227340c208def69c10660c2fab0a1
Corey Frang <gnarf@gnarf.net>
2015-03-10 14:19:01 -0400

    Remove test stub for no longer present wording in the spec

--- test/integration/json-api/v1/base-format/read/index.js
+++ test/integration/json-api/v1/base-format/read/index.js
@@ -272,10 +272,6 @@ describe('read', function() {
           bookRouteHandler(readReq);
         });
 
-        // https://github.com/json-api/json-api/commit/110cc87bd069d8d2ee590f1b1199af3c9316ee58
-        it('may include a string in its links object keyed by "resource"');
-
-
         // TODO: API TEST
         // it('must respond to a get request to any `self` url with the resource as primary data');
       });
