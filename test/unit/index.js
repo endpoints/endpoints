e9908f7b497806ab4967bf668646fe29f9175ec9
Ollie Relph <ollie@relph.me>
2016-01-29 15:29:06 +0000

    Add payload handler tests back

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,7 +1,7 @@
 describe('Unit', function () {
   require('./application');
   require('./controller');
-  //require('./payload-handler/test');
+  require('./payload-handler');
   require('./format-jsonapi');
   require('./request-handler');
   require('./store-bookshelf');
