a83e11030526053becae950a637c2a889baf7ae4
Ollie Relph <ollie@relph.me>
2016-02-03 12:24:26 +0000

    Correct test schemas to validate correctly

--- test/app/src/modules/authors/schema.js
+++ test/app/src/modules/authors/schema.js
@@ -1,19 +1,41 @@
 module.exports = {
   body: {
+    required: true,
+    type: 'object',
     properties: {
-      id: {
-        type: 'integer'
+      data: {
+        required: true,
+        type: 'object',
+        properties: {
+          type: {
+            required: true,
+            type: 'string'
+          },
+          attributes: {
+            required: true,
+            type: 'object',
+            properties: {
+              id: {
+                type: 'integer'
+              },
+              name: {
+                type: 'string'
+              },
+              date_of_birth: {
+                type: 'string',
+                format: 'date-time'
+              },
+              date_of_death: {
+                type: 'string',
+                format: 'date-time'
+              }
+            }
+          }
+        }
       },
-      name: {
-        type: 'string'
-      },
-      date_of_birth: {
-        type: 'string',
-        format: 'date-time'
-      },
-      date_of_death: {
-        type: 'string',
-        format: 'date-time'
+      relationships: {
+        required: false,
+        type: 'object',
       }
     }
   }
--- test/app/src/modules/books/schema.js
+++ test/app/src/modules/books/schema.js
@@ -1,20 +1,43 @@
 module.exports = {
   body: {
+    required: true,
+    type: 'object',
     properties: {
-      id: {
-        type: 'integer'
-      },
-      title: {
-        type: 'string'
-      },
-      ordering: {
-        type: 'string',
-        format: 'date-time'
-      },
-      links: {
-        author: {
-          type: 'string'
+      data: {
+        required: true,
+        type: 'object',
+        properties: {
+          type: {
+            required: true,
+            type: 'string'
+          },
+          attributes: {
+            required: true,
+            type: 'object',
+            properties: {
+              id: {
+                required: false,
+                type: 'integer'
+              },
+              title: {
+                type: 'string'
+              },
+              ordering: {
+                type: 'string',
+                format: 'date-time'
+              },
+              links: {
+                author: {
+                  type: 'string'
+                }
+              }
+            }
+          }
         }
+      },
+      relationships: {
+        required: false,
+        type: 'object',
       }
     }
   }
